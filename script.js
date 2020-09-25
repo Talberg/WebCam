8ec9b45f1091d999b2af91ab1446fd0832456008
Alexander Talberg <alexandermtalberg@gmail.com>
2020-09-25 10:42:25 -0600

    back camera

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const video = document.getElementById('video')
 function startVideo (){
     navigator.getUserMedia(
         {video:{
-            facingMode:'environment'
+            width: {
+                min: 1280,
+                ideal: 1920,
+                max: 2560,
+              },
+              height: {
+                min: 720,
+                ideal: 1080,
+                max: 1440
+              },
+              facingMode: 'environment'
         }},
         stream=>video.srcObject =stream,
         err=>console.error(err)
